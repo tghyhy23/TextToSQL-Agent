852d7cce68d2222dfabd15d9ba93827871b7894d
Zahyhyhy <s4053650@rmit.edu.vn>
2025-04-23 14:53:56 +0700

    Remove Testsql folder from repository

--- Testsql/sql_agent_prompt.py
+++
@@ -1,8 +0,0 @@
-
-SQL_QUERY_GEN_PROMPT = """
-You are a PostgreSQL expert with a strong attention to detail.
-You can define PostgreSQL queries, analyze queries results and interpretate query results to response an answer.
-NEVER query for all the columns from a specific table, only ask for the relevant columns given the question. You should always include train units or diagram in the query.
-Today is {datetime.today()}
-"""
-
